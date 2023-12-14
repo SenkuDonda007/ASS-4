597baed28840689dbaa4bbabce920426e8f6c0df
SenkuDonda007 <kevadiyahet1234@gmail.com>
2023-12-13 21:27:04 -0500

    1

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var studentInfo = document.getElementById("student-info");
+    var studentId = "200520505";  
+    var studentName = "Het";     
+
+    studentInfo.textContent = "Student ID: " + studentId + " | Name: " + studentName;
+});
